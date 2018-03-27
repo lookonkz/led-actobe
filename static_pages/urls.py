61dde4fed459f49244c74722f4e38805b93569a9
user1 <user1@iMac-user1.local>
2018-03-27 16:39:17 +0600

    v -1.1.4.1

--- static_pages/urls.py
+++ static_pages/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import PageDetail
+from django.views.generic.base import TemplateView
 
 app_name = 'static_pages'
 urlpatterns = [
     path('page/<slug:slug>', PageDetail.as_view(), name='page'),
+    path('politic/', TemplateView.as_view(template_name='static_pages/politic.html'), name='politic'),
 
 ]
